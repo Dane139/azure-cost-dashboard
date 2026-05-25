55bbfb9b7761085b0be3b4832e2790cfab226a13
Dane139 <dane.willms1@gmail.com>
2026-05-25 09:00:30 -0400

    Revert "Adjusted budget to a lower threshold"
    This reverts commit 419020d7cc7f2aeb8ff2b650fc6f66ac893808e2.

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ resource "azurerm_consumption_budget_subscription" "main" {
   name            = "budget-cost-${var.yourname}"
   subscription_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
  
-  amount     = 50
+  amount     = 200
   time_grain = "Monthly"
  
   time_period {
