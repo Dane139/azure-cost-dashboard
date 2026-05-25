419020d7cc7f2aeb8ff2b650fc6f66ac893808e2
Dane139 <dane.willms1@gmail.com>
2026-05-24 23:40:34 -0400

    Adjusted budget to a lower threshold

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ resource "azurerm_consumption_budget_subscription" "main" {
   name            = "budget-cost-${var.yourname}"
   subscription_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
  
-  amount     = 200
+  amount     = 50
   time_grain = "Monthly"
  
   time_period {
